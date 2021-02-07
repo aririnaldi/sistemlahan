d940004b2ee1d6089ffd9073e0140d36057c6dcc
aririnaldi <rinaldiari38@gmail.com>
2021-02-06 23:16:56 -0800

    ngasih enter

--- home.php
+++ home.php
@@ -14,6 +14,7 @@ session_start();
 <html>
 
 <head>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
