83319e1b5e477e15e6d4e93042d4a51c124ff6ad
masdikdok <dicki123andrea@gmail.com>
2021-02-07 14:52:07 +0700

    update file koneksi di branch dicki

--- koneksi.php
+++ koneksi.php
@@ -4,18 +4,19 @@ $host="localhost";
 $user="root";
 $password="";
 $database="lahan";
+// contoh
 
 
-$connect=mysqli_connect($host,$user,$password,$database); 
+$connect=mysqli_connect($host,$user,$password,$database);
 if(!$connect)
 {
 	echo "acces denied";
 }
 
 mysqli_select_db($connect,"lahan")
-or die( "Tidak ada database "); 
+or die( "Tidak ada database ");
 
-  
+
 error_reporting();
 
 ?>
